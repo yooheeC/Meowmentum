ab7dd05042a2d0e27abd89fd9fc6a06f34047a52
yooheeC <87816430+yooheeC@users.noreply.github.com>
2024-12-15 21:07:00 +0100

    Add files via upload

--- js/quotes.js
+++ js/quotes.js
@@ -1,33 +1,33 @@
 const quotes = [
-    { quote: "the world is my meow meow.",
-        author: "meow"
+    { quote: "the world is my meow meow. ",
+        author: "–meow"
     },
-    { quote: "The only thing we have to meow is meow itself.",
-        author: "-Franklin D. Meowmeow"
+    { quote: "The only thing we have to meow is meow itself. ",
+        author: "–Franklin D. Meowmeow"
     },
-    { quote: "Do one thing every day that meow you",
-        author: "Meow Roosevelt"
+    { quote: "Do one thing every day that meow you. ",
+        author: "–Meow Roosevelt"
     },
-    { quote: "Meow done is better than meow said.",
-        author: "Benjamin meow"
+    { quote: "Meow done is better than meow said. ",
+        author: "–Benjamin meow"
     },
-    { quote: "Be meow meow; everyone else is already taken.",
-        author: "Oscar Meow"
+    { quote: "Be meow meow; everyone else is already taken. ",
+        author: "–Oscar Meow"
     },
-    { quote: "You must be the meow you wish to see in the world.",
-        author: "Mahatma Meow"
+    { quote: "You must be the meow you wish to see in the world. ",
+        author: "–Mahatma Meow"
     },
-    { quote: "Spread meow everywhere you go. Let no one ever come to you without meow happier",
-        author: "Meow Teresa"
+    { quote: "Spread meow everywhere you go. Let no one ever come to you without meow happier. ",
+        author: "–Meow Teresa"
     },
-    { quote: "The way to get started is to quit talking and begin meowing.",
-        author: "Meow Disney"
+    { quote: "The way to get started is to quit talking and begin meowing. ",
+        author: "–Meow Disney"
     },
-    { quote: "Never let the meow of striking out keep you from playing the game.",
-        author: "Babe Meow"
+    { quote: "Never let the meow of striking out keep you from playing the game. ",
+        author: "–Babe Meow"
     },
-    { quote: "Only a meow meowed for others is a life worthwhile.",
-        author: "Meow Einstein"
+    { quote: "Only a meow meowed for others is a life worthwhile. ",
+        author: "–Meow Einstein"
     },
 ]
 
